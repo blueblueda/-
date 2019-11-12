89ccc0312d0cf633781b90b27e10d28bf3c1cab4
blueblueda <359068869@qq.com>
2019-11-12 20:51:15 +0800

    求9

---
+++ 求9/求9/求9.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main()
+{
+	int count = 0;
+	for (int i = 1; i < 100; i++)
+	{
+		if ((i % 10 == 9))
+			count += 1;
+		if (i / 10 == 9)
+			count += 1;
+	}
+	
+
+
+	printf("%d\n", count);
+	return 0;
+}
