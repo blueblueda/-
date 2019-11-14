96413567a7a60a4ec86eea33b68c3d4f3fc77978
blueblueda <359068869@qq.com>
2019-11-14 20:31:48 +0800

    猜密码

---
+++ 密码/密码/密码.cpp
@@ -0,0 +1,27 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#define times 3
+int main() {
+	char password[10] = { 0 };
+	int i = 0;
+	printf("请输入密码\n");
+	for (i; i < times; ++i) {
+		scanf("%s", password);
+
+		if (strcmp(password, "123456") == 0) {
+			break;
+		}
+		else {
+			printf("密码错误，重新输入\n");
+		}
+	}
+	if (i == times) {
+		printf("退出系统\n");
+	}
+	else {
+		printf("登入成功\n");
+	}
+	
+	return 0;
+}
